10fa265825b43ef0083477291d7d6231e5f10adf
Ulises Santana <ulisesantana@gmail.com>
2018-02-05 20:05:54 +0000

    Add CRUD for Employee

--- src/routes/Employee.ts
+++ src/routes/Employee.ts
@@ -25,36 +25,65 @@ export default class EmployeeRouter {
     this.router.get('/', this.getEmployees.bind(this));
     this.router.post('/', this.saveEmployee.bind(this));
     this.router.get('/:id', this.getEmployeeById.bind(this));
-    this.router.put('/:id', this.updateEmployee.bind(this));
+    this.router.patch('/:id', this.updateEmployee.bind(this));
     this.router.delete('/:id', this.deleteEmployee.bind(this));
   }
 
   private async getEmployeeById(ctx: Context) {
-    ctx.body = 'Hola mundo!';
-    console.log(this);
-  }
-  
-  private async getEmployees(ctx: Context) {
-    console.log(this);
     try {
-      ctx.body = await this.repository.find({}); 
+      ctx.body = await this.repository.findOneById(ctx.params.id); 
+      if (!ctx.body){
+        ctx.status = 404;
+      } 
     } catch (err) {
       ctx.body = err;
       ctx.status = 500;
     }
-
   }
-
-  private saveEmployee(ctx: Context) {
-
-  }
-
-  private updateEmployee(ctx: Context) {
   
+  private async getEmployees(ctx: Context) {
+    try {
+      ctx.body = await this.repository.find({}); 
+      if (ctx.body.length < 1){
+        ctx.status = 404;
+      } 
+    } catch (err) {
+      ctx.body = err;
+      ctx.status = 500;
+    }
+    
   }
-
-  private deleteEmployee(ctx: Context) {
-
+  
+  private async saveEmployee(ctx: Context) {
+    try {
+      const employee: Employee = new Employee();
+      employee.name = ctx.request.body.name;
+      ctx.body = await this.repository.save(employee); 
+    } catch (err) {
+      ctx.body = err;
+      ctx.status = 500;
+    }
+  }
+  
+  private async updateEmployee(ctx: Context) {
+    try {
+      ctx.body = await this.repository.updateById(
+        ctx.params.id, 
+        ctx.request.body
+      ); 
+    } catch (err) {
+      ctx.body = err;
+      ctx.status = 500;
+    }
+  }
+  
+  private async deleteEmployee(ctx: Context) {
+    try {
+      ctx.body = await this.repository.removeById(ctx.params.id);
+    } catch (err) {
+      ctx.body = err;
+      ctx.status = 500;
+    }
   }
 
 }

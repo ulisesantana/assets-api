a2f05811d0188ed8a519f1fe4c2bf3559ec06343
Ulises Santana <ulisesantana@gmail.com>
2018-02-11 03:46:02 +0000

    Add CRUD functionality to Asset

--- src/routes/Asset.ts
+++ src/routes/Asset.ts
@@ -1,15 +1,16 @@
 import { Context } from 'koa';
 import * as Router from "koa-router";
-import { getManager, Repository, Connection } from "typeorm";
+import { Connection, EntityManager } from "typeorm";
 import { Asset } from '../entities/Asset';
+import { Record } from '../entities/Record';
 
 export default class AssetRouter {
   public router: Router;
-  private repository: Repository<Asset>
+  private manager: EntityManager
 
   constructor(connection: Connection) {
-    this.repository = connection.getRepository(Asset);
     this.router = new Router({ prefix: '/assets' });
+    this.manager = connection.manager;
   }
 
   public routes() {
@@ -22,16 +23,16 @@ export default class AssetRouter {
   }
 
   private addRoutes(): void {
-    this.router.get('/', this.getAssets.bind(this));
-    this.router.post('/', this.saveAsset.bind(this));
-    this.router.get('/:id', this.getAssetById.bind(this));
-    this.router.patch('/:id', this.updateAsset.bind(this));
-    this.router.delete('/:id', this.deleteAsset.bind(this));
+    this.router.get('/', this.getAll.bind(this));
+    this.router.post('/', this.save.bind(this));
+    this.router.get('/:id', this.getById.bind(this));
+    this.router.patch('/:id', this.updateById.bind(this));
+    this.router.delete('/:id', this.deleteById.bind(this));
   }
 
-  private async getAssetById(ctx: Context) {
+  private async getById(ctx: Context) {
     try {
-      ctx.body = await this.repository.findOneById(ctx.params.id);
+      ctx.body = await this.manager.findOneById(Asset, ctx.params.id);
       if (!ctx.body) {
         ctx.status = 404;
       }
@@ -41,9 +42,9 @@ export default class AssetRouter {
     }
   }
 
-  private async getAssets(ctx: Context) {
+  private async getAll(ctx: Context) {
     try {
-      ctx.body = await this.repository.find({});
+      ctx.body = await this.manager.find(Asset, {});
       if (ctx.body.length < 1) {
         ctx.status = 404;
       }
@@ -54,17 +55,69 @@ export default class AssetRouter {
 
   }
 
-  private async saveAsset(ctx: Context) {
-
-  }
-
-  private async updateAsset(ctx: Context) {
-
-  }
-
-  private async deleteAsset(ctx: Context) {
+  private async save(ctx: Context) {
     try {
-      ctx.body = await this.repository.removeById(ctx.params.id);
+      await this.manager.transaction(async manager => {
+        const asset: Asset = new Asset();
+        asset.name = ctx.request.body.name || null;
+        asset.notes = ctx.request.body.notes || null;
+        asset.blocked = ctx.request.body.blocked || false;
+        asset.serialNumber = ctx.request.body.serialNumber || null;
+
+        ctx.body = await manager.save(asset);
+
+        const record: Record = new Record();
+        record.assetId = asset.id;
+
+        await manager.save(record);
+      });
+
+    } catch (err) {
+      ctx.body = { message: err.message };
+      ctx.status = err.status || 500;
+    }
+  }
+
+  private async updateById(ctx: Context) {
+    try {
+      await this.manager.transaction(async manager => {
+        let id = ctx.params.id;
+        let assetUpdate = ctx.request.body;
+
+        if (assetUpdate.employeeId) {
+          const asset = await this.manager.findOneById(Asset, id);
+          ctx.body = await this.manager.updateById(
+            Asset,
+            id,
+            assetUpdate
+          );
+
+          const record: Record = new Record();
+          record.assetId = id;
+          record.fromEmployeeId = asset.employeeId;
+          record.toEmployeeId = assetUpdate.employeeId;
+
+          await manager.save(record);
+        } else {
+          ctx.body = await this.manager.updateById(
+            Asset,
+            id,
+            assetUpdate
+          );
+        }
+
+      });
+    } catch (err) {
+      ctx.body = { message: err.message };
+      ctx.status = err.status || 500;
+    }
+  }
+
+  private async deleteById(ctx: Context) {
+    try {
+      await this.manager.transaction(async manager => {
+        ctx.body = await this.manager.removeById(Asset, ctx.params.id);
+      });
     } catch (err) {
       ctx.body = { message: err.message };
       ctx.status = err.status || 500;

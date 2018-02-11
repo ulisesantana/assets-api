09f25736f277bc00ddf8d91f84b0440f2cd6c1d6
Ulises Santana <ulisesantana@gmail.com>
2018-02-11 03:46:18 +0000

    Update basic test

--- test/basic.test.ts
+++ test/basic.test.ts
@@ -4,7 +4,6 @@ describe('Basic tests', function () {
 
   it('Should be true', function () {
     assert.equal(1, true);
-    assert.notDeepEqual(1, true);
     assert.deepEqual(true, true);
   });
 
